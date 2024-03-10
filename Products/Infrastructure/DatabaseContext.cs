5dae279a156f969aafca911193b74eeb54d08c59
Philip Zadeh <90150224+PhilEZad@users.noreply.github.com>
2024-03-10 19:33:05 +0100

    Added db context for product

--- Products/Infrastructure/DatabaseContext.cs
+++ Products/Infrastructure/DatabaseContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure;
 
@@ -10,6 +11,14 @@ public class DatabaseContext : DbContext
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Product>()
+            .HasKey(i => i.Id)
+            .HasName("PK_Product");
         
+        modelBuilder.Entity<Product>()
+            .Property( i => i.Id)
+            .ValueGeneratedOnAdd();
     }
+    
+    public DbSet<Product> Products { get; set; }
 }
