fe5222d4f0392e82e5210cb8c999bc76025c7216
Philip Zadeh <PhilEZad@gmail.com>
2024-03-12 11:44:03 +0100

    Fixed weird .net 8 injection

--- Products/API/Controllers/ProductController.cs
+++ Products/API/Controllers/ProductController.cs
@@ -7,9 +7,14 @@ namespace API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class ProductController(IProductService productService) : ControllerBase
+public class ProductController : ControllerBase
 {
-    private readonly IProductService _productService = productService;
+    private readonly IProductService _productService;
+        
+    public ProductController(IProductService productService)
+    {
+        _productService = productService;
+    }
 
     [HttpGet]
     public ActionResult GetProducts()
